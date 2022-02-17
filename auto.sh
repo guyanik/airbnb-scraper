b57fdddd3fe4989aeee42180865f7a77e750b8a4
hguyanik <uyanikhg@gmail.com>
2022-02-17 18:39:44 +0300

    Created and edited for a client

--- auto.sh
+++ auto.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 input="towns.txt"
 now=$(date +'%Y-%m-%d')
-
-scrapy crawl airbnb -a query="Abercrombie Caves, NSW, Australia" -o "weekly/australia-$now.csv" -t headless
+while IFS= read -r line
+do
+  echo "$line"
+  scrapy crawl airbnb -a query="$line, Australia" -o "weekly/australia-$now.csv" -t headless
+done < "$input"
 python photo.py
